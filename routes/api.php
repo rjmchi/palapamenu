03bfd108fef13d54cc0f5e0ab9ef04bac9377b5d
rjmchi <robert@rjmprogramming.com>
2020-11-02 11:07:56 -0600

    Adds routes for API

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('menu', 'MenuController');
+Route::resource('category', 'CategoryController');
+Route::resource('item', 'ItemController');
+Route::resource('option', 'OptionController');
+Route::resource('choice', 'ChoiceController');
