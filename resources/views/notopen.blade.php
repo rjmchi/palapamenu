79ebb3d08f9b7e824d48b11a762211e301006938
rjmchi <robert@rjmprogramming.com>
2021-05-30 20:16:33 -0500

    Changes format for closed message

--- resources/views/notopen.blade.php
+++ resources/views/notopen.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="card">
-        <div class="card-header">{{ __('Login') }}    
+        <div class="card-header">
             <h4>{{__('The Palapa is now closed for on-line orders')}}</h4>
         </div>
     </div>
